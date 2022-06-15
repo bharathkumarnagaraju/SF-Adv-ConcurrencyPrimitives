f6545e3c93771b3ec104784ea91106feef665d12
simon <simon@dancingcloudservices.com>
2022-06-15 09:55:33 -0600

    Break 2

---
+++ src/main/java/runnables/BadTiming.java
@@ -0,0 +1,25 @@
+package runnables;
+
+public class BadTiming {
+  static boolean stop = false;
+
+  public static void main(String[] args) throws InterruptedException {
+    new Thread(() -> {
+      System.out.println("Worker " + Thread.currentThread().getName()
+          + " started");
+      while (!stop)
+        ;
+      System.out.println("Worker " + Thread.currentThread().getName()
+          + " finished!!!!");
+    }).start();
+
+    System.out.println(Thread.currentThread().getName() +
+        " worker started, pausing...");
+    Thread.sleep(1_000);
+    System.out.println(Thread.currentThread().getName() +
+        " about to set stop flag...");
+    stop = true;
+    System.out.println(Thread.currentThread().getName() +
+        " stop flag is " + stop + " main exiting");
+  }
+}
